5beb0adbced7f2b04503fbef6e68f5368fb1ae2c
joaoprosa <jp.ragdool@hotmail.com>
2025-02-01 16:41:36 -0300

    added in construction page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,20 @@
+import "bulma/css/bulma.css"
+
 export default function Home() {
-  return <h1>Não é um teste</h1>
+  return (
+    <div className="container py-5 px-5">
+      <div className="is-flex is-flex-direction-column is-justify-content-center is-align-items-center has-text-centered py-5">
+        <h1 className="title">Em construção</h1>
+        <p class="subtitle">Em breve um novo pedaço da internet aqui :)</p>
+      </div>
+
+      <div className="skeleton-lines mt-5">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  )
 }
