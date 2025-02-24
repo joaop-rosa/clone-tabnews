8acc3f7b07a94e83295941abccddd17820bba6a2
joaoprosa <jp.ragdool@hotmail.com>
2025-02-24 20:28:31 -0300

    added tests and migrations

---
+++ tests/integration/api/v1/migrations/get.test.js
@@ -0,0 +1,13 @@
+const { cleanDatabase } = require("../helpers/database")
+
+beforeAll(cleanDatabase)
+
+test("GET to /api/v1/migrations should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations")
+  expect(response.status).toBe(200)
+
+  const responseBody = await response.json()
+
+  expect(Array.isArray(responseBody)).toBe(true)
+  expect(responseBody.length).toBeGreaterThan(0)
+})
