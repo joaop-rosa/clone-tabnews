026cd4603fd79d242d2e2a48bfdfbab59e339ca5
joaoprosa <jp.ragdool@hotmail.com>
2025-03-13 19:49:44 -0300

    test: improve test structure

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,15 +4,19 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices()
 })
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status")
-  expect(response.status).toBe(200)
+describe("GET to /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status")
+      expect(response.status).toBe(200)
 
-  const responseBody = await response.json()
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString()
+      const responseBody = await response.json()
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString()
 
-  expect(responseBody.updated_at).toEqual(parsedUpdatedAt)
-  expect(responseBody.dependencies.database.pg_max_connections).toEqual(100)
-  expect(responseBody.dependencies.database.pg_used_connections).toEqual(1)
-  expect(responseBody.dependencies.database.pg_version).toBe("16.0")
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt)
+      expect(responseBody.dependencies.database.pg_max_connections).toEqual(100)
+      expect(responseBody.dependencies.database.pg_used_connections).toEqual(1)
+      expect(responseBody.dependencies.database.pg_version).toBe("16.0")
+    })
+  })
 })
