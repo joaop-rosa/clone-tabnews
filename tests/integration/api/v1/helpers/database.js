e998424b6cb95d84095207922e010ff3925913de
joaoprosa <jp.ragdool@hotmail.com>
2025-04-01 20:01:24 -0300

    refactor: improve test files organization

--- tests/integration/api/v1/helpers/database.js
+++
@@ -1,7 +0,0 @@
-import database from "infra/database"
-
-async function cleanDatabase() {
-  await database.query("drop schema public cascade; create schema public;")
-}
-
-export { database, cleanDatabase }
--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,4 +1,6 @@
 import retry from "async-retry"
+import database from "infra/database"
+import { runPendingMigrations as modelRunPendingMigrations } from "models/migrator"
 
 async function waitForAllServices() {
   await waitForWebServer()
@@ -16,6 +18,14 @@ async function waitForAllServices() {
   }
 }
 
-const orchestrator = { waitForAllServices }
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;")
+}
+
+async function runPendingMigrations() {
+  await modelRunPendingMigrations()
+}
+
+const orchestrator = { waitForAllServices, cleanDatabase, runPendingMigrations }
 
 export default orchestrator
